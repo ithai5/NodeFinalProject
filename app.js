169f84ee1c6f0d23e8237bb4ee42b4656a2bc2ee
Itai Gramse <ithai.g@gmail.com>
2021-05-05 11:35:20 +0300

    add router and fs

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ const app = express();
 app.use(express.json());
 app.use(express.static("public"));
 
+const postsRoutes = require("./routes/posts");
+const usersRoutes = require("./routes/users"); 
+
+const fs = require("fs");
+
+const test = fs.readFileSync(__dirname + "/public/test.html", "utf-8");
+const footer = fs.readFileSync(__dirname + "/public/footer/footer.html", "utf-8");
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/frontpage.html");
+    res.send(test + footer);
 });
 
 
@@ -18,3 +25,4 @@ const PORT = process.env.PORT || 8080
 app.listen(PORT, err => {
     err? console.log(err) : console.log('App runs on port: ', Number(PORT))
 });
+
