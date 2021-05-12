cc53013d28ef94e734450a46fc36c1a3a8aa4e25
Plushoe <thomas.anastacio@gmail.com>
2021-05-12 10:18:27 +0200

    Mongodb starting steps

---
+++ mongo.js
@@ -0,0 +1,2 @@
+const MongoClient = require("mongodb").MongoClient;
+const url = "mongodb://den1.mongo1.gear.host/nodefinalproject:27001";
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,22 @@
 const router = require('express').Router()
 
 
+router.get("/login", (req, res) => {
+
+})
+
+router.post("/api/login", (req, res) => {
+    const loginInfo = {... req.body};
+    
+    //condition needs to be connected to the database
+    const condition = true;
+
+    if (condition) {
+        //Send back a cookie and expiration,
+        //Something identifying the logged in user (id?)
+        res.send()
+    }
+})
 
 
 module.exports = {
