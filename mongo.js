5796efff7a347303933989d1b0055c7ba343b68f
Plushoe <thomas.anastacio@gmail.com>
2021-05-12 11:04:22 +0200

    Mongo connection established! Added dotenv dependency

--- mongo.js
+++
@@ -1,2 +0,0 @@
-const MongoClient = require("mongodb").MongoClient;
-const url = "mongodb://den1.mongo1.gear.host/nodefinalproject:27001";
---
+++ mongoTest.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const MongoClient = require("mongodb").MongoClient;
+const url = process.env.DB_CONNECTION;
+const dbName = "NodeExam"
+
+MongoClient.connect(url, {useUnifiedTopology: true}, (error, client) => {
+    if (error) {
+        throw new Error(error);
+    }
+
+    const db = client.db(dbName);
+    const users = db.collection("users");
+
+    //Testing if it works
+    users.findOne( {"userName":"IgnaDa1"}, (error, result) => {
+        if (error) {
+            throw new Error(error);
+        }
+
+        console.log(result);
+        client.close();
+    });
+});
