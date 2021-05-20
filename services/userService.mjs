79995acf1dea236c4a68153469bad4724ba3cdd2
Plushoe <thomas.anastacio@gmail.com>
2021-05-20 12:05:56 +0200

    Fixed signup so that it hashes the password

--- services/userService.mjs
+++ services/userService.mjs
@@ -1,17 +1,19 @@
 import { promiseGet, promiseCreate } from "./dbService.mjs";
-import passwordManagment from "../passwordManagment.mjs"
+import passwordManagement from "../passwordManagement.mjs"
 
 const USERS = "users"; 
 async function getUser(query){ 
-    const  newHahedPassword = await passwordManagment.passwordToHash(query.password)
+    const  newHahedPassword = await passwordManagement.passwordToHash(query.password)
     // query = { email: test@test.dk , password: "123456"}
     //send db query to get user hashed password in return
     const user = await promiseGet(USERS,  { email : query.email });
     //compare the query password with the hashed db password
-    return await passwordManagment.compareHash(query.password, user[0].password);
+    return await passwordManagement.compareHash(query.password, user[0].password);
 }
 
 async function signUp(signUpInfo) {
+    signUpInfo.password = await passwordManagement.passwordToHash(signUpInfo.password);
+
     return await (promiseCreate(USERS, signUpInfo));
 }
 
