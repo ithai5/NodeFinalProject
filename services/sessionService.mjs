a4cc1befa006bd11ad5a12dcf22ef463b6ba62d3
Itai Gramse <ithai.g@gmail.com>
2021-06-16 12:35:31 +0200

    add max age for session
    session will be expired in 7 days

--- services/sessionService.mjs
+++ services/sessionService.mjs
@@ -8,7 +8,7 @@ function createSession() {
         secret: process.env.SECRET,
         resave: false,
         saveUninitialized: true,
-        cookie: { secure: false },
+        cookie: { maxAge: 1000*60*60*24*7 }, //1000mil * 60 seconds * 60 minuts * 24 hours * 7 days 
         store: MongoStore.create({ mongoUrl: process.env.DB_CONNECTION, dbName: "NodeExam", collection: "session" })
     });
 }
